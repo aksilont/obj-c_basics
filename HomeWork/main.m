21d3b51a091d9338569de777eb7e2e6fdaa044f9
aksilont <aksilont@gmail.com>
2020-11-26 11:02:02 +0300

    Home work 1

--- HomeWork/main.m
+++ HomeWork/main.m
@@ -8,48 +8,52 @@
 #import <Foundation/Foundation.h>
 #import <stdlib.h>
 
+void homeWork1() {
+    // Task 1, 2
+    int first;
+    int second;
+    
+    printf("First number: ");
+    scanf("%d", &first);
+    printf("Second number: ");
+    scanf("%d", &second);
+    
+    int resultOfSum = first + second;
+    int resultOfSubtract = first - second;
+    int resultOfMultiply = first * second;
+    float resultOfDivide = (float)first / (float)second;
+    
+    NSLog(@"\n---------- TASK 1, 2 ----------"
+          "\nFirst number is %d"
+          "\nSecond number is %d"
+          "\n----------------------------"
+          "\nResult of sum = %d"
+          "\nResult of subtract = %d"
+          "\nResult of multiply = %d"
+          "\nResult of divide = %f"
+          "\n----------------------------",
+          first, second, resultOfSum, resultOfSubtract, resultOfMultiply, resultOfDivide);
+    
+    // Task 3
+    int firstRnd = arc4random_uniform(100);
+    int secondRnd = arc4random_uniform(100);
+    int thirdRnd = arc4random_uniform(100);
+    
+    float average = ((float)firstRnd + (float)secondRnd + (float)thirdRnd) / 3.0;
+    NSLog(@"\n---------- TASK 3 ----------"
+          "\nFirst random number is %d"
+          "\nSecond random number is %d"
+          "\nThird random number is %d"
+          "\n----------------------------"
+          "\nAverage is %f"
+          "\n----------------------------",
+          firstRnd, secondRnd, thirdRnd, average);
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-                
-        // Task 1, 2
-        int first;
-        int second;
-        
-        printf("First number: ");
-        scanf("%d", &first);
-        printf("Second number: ");
-        scanf("%d", &second);
-        
-        int resultOfSum = first + second;
-        int resultOfSubtract = first - second;
-        int resultOfMultiply = first * second;
-        float resultOfDivide = (float)first / (float)second;
-        
-        NSLog(@"\n---------- TASK 1, 2 ----------"
-              "\nFirst number is %d"
-              "\nSecond number is %d"
-              "\n----------------------------"
-              "\nResult of sum = %d"
-              "\nResult of subtract = %d"
-              "\nResult of multiply = %d"
-              "\nResult of divide = %f"
-              "\n----------------------------",
-              first, second, resultOfSum, resultOfSubtract, resultOfMultiply, resultOfDivide);
-        
-        // Task 3
-        int firstRnd = arc4random_uniform(100);
-        int secondRnd = arc4random_uniform(100);
-        int thirdRnd = arc4random_uniform(100);
-        
-        float average = ((float)firstRnd + (float)secondRnd + (float)thirdRnd) / 3.0;
-        NSLog(@"\n---------- TASK 3 ----------"
-              "\nFirst random number is %d"
-              "\nSecond random number is %d"
-              "\nThird random number is %d"
-              "\n----------------------------"
-              "\nAverage is %f"
-              "\n----------------------------",
-              firstRnd, secondRnd, thirdRnd, average);
+        homeWork1();
     }
     return 0;
 }
