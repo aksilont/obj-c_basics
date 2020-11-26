b0c7aeba7bb25cf5a00b754c177a56125276a7ec
aksilont <aksilont@gmail.com>
2020-11-26 10:50:22 +0300

    Initial Commit

---
+++ HomeWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeWork
+//
+//  Created by Sky on 26.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
