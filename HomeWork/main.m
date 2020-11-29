947b31f198c2b1e1ba6b5ed7d8ef0e025fd387e1
aksilont <aksilont@gmail.com>
2020-11-29 20:42:18 +0300

    Home work 3

--- HomeWork/main.m
+++ HomeWork/main.m
@@ -7,10 +7,57 @@
 
 #import <Foundation/Foundation.h>
 
+void task1() {
+    NSString *world = @"World";
+    NSArray *arr = @[@"Hello", @", ", world, @"!"];
+    
+    for (NSString *item in arr) {
+        NSLog(@"%@", item);
+    }
+}
+
+void task2() {
+    
+}
+
+void task3() {
+    
+    enum Gender {
+        Male,
+        Female
+    };
+    typedef enum Gender Gender;
+    
+    typedef NS_ENUM(NSString, MyEnum) {
+        Male = @"male",
+        Female = @"female"
+    };
+    
+    struct Human {
+        NSString *name;
+        NSInteger age;
+        Gender gender;
+    };
+    typedef struct Human Human;
+    
+    Human Ann;
+    Ann.name = @"Ann";
+    Ann.age = 21;
+    Ann.gender = Male;
+    
+    NSLog(@"Name - %@, age - %li, gender - %u", Ann.name, (long)Ann.age, Ann.gender);
+    
+}
+
+void homeWork3() {
+//    task1();
+//    task2();
+    task3();
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        homeWork3();
     }
     return 0;
 }
